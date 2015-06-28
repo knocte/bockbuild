a136ef2251011a60005d72e7bc1ebd61b376acda
David Nielsen <gnomeuser@gmail.com>
2015-06-29 00:33:52 +0200

    [libogg] Fix libogg build by upgrading it to 1.3.2
    Build for libogg was failing with this warnings and
    error:
    
    aclocal: warning: autoconf input should be named 'configure.ac', not 'configure.in'
    configure.in:18: warning: macro 'AM_PROG_LIBTOOL' not found in library
    configure.in:22: warning: macro 'AM_CONFIG_HEADER' not found in library
    configure.in:18: error: possibly undefined macro: AM_PROG_LIBTOOL
          If this token and others are legitimate, please use m4_pattern_allow.
          See the Autoconf documentation.
    configure.in:22: error: possibly undefined macro: AM_CONFIG_HEADER
    
    By upgrading to 1.3.2 we fix this and we don't need a custom
    patch anymore.
    
    The only profile that was using libogg was banshee's.

--- packages/libogg.py
+++ packages/libogg.py
@@ -1,18 +1,1 @@
-class LiboggPackage (XiphPackage):
-        def __init__ (self):
-                XiphPackage.__init__ (self,
-                        project  = 'ogg',
-			name = 'libogg',
-                        version = '1.3.0')
-
-               self.package_prefix = self.profile.prefix
-               self.configure = 'autoreconf -fi && ./configure --prefix="%{package_prefix}"'
-
-		# reduce optimization from -O4 to -O3 to allow compilation on Xcode 4.2.1
-                self.sources.append ('patches/libogg-opt.patch')
-
-        def prep (self):
-                Package.prep (self)
-                self.sh ('patch -p1 < "%{local_sources[1]}"')
-
-LiboggPackage ()
+XiphPackage ('ogg', 'libogg', '1.3.2')
