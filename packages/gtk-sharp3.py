e5de33b15c493a8e89637a7c847be06b0bb7eac7
David Nielsen <gnomeuser@gmail.com>
2015-06-28 16:47:31 +0200

    [packages] Add gtk-sharp3 2.99.3.99
    Add a gtk-sharp3 package from git, versioned 2.99.3.99 to indicate it
    is pre 2.99.4.
    
    Also add patch from https://github.com/mono/gtk-sharp/pull/130 to fix
    compilation on OS X.

---
+++ packages/gtk-sharp3.py
@@ -0,0 +1,28 @@
+class GtkSharp3Package (GitHubPackage):
+	def __init__ (self):		
+		GitHubPackage.__init__ (self, 'mono', 'gtk-sharp', '2.99.3.99',
+			revision = 'a3db272fee017518779344293fb802cc8d1f813b')
+
+		if Package.profile.name == 'darwin':
+			self.sources.extend ([
+				# Fix compilation on OS X
+				# https://github.com/mono/gtk-sharp/pull/130
+				'https://github.com/mono/gtk-sharp/commit/89b2dae6d82bfb22ee8641e5bbd6d42dcbab8c17.patch'
+			])
+
+		if Package.profile.name == 'darwin' and not Package.profile.m64:
+			self.configure_flags.extend ([
+				# fix building i386 on x86_64
+				'--build=i386-apple-darwin11.2.0',
+			])
+
+	def prep (self):
+		Package.prep (self)
+		if Package.profile.name == 'darwin':
+			for p in range (1, len (self.sources)):
+				self.sh ('patch -p1 --ignore-whitespace < "%{sources[' + str (p) + ']}"')
+				
+		# Run autogen, the correct flags will be picked automatically up when configure is run.
+		self.sh ('./autogen.sh --prefix="%{prefix}"')
+
+GtkSharp3Package ()
