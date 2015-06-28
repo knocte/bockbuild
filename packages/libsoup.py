a289cd42bed4a42a0711e415ad4260162d11761e
Andrés G. Aragoneses <knocte@gmail.com>
2015-06-28 11:08:45 +0200

    [libsoup] add --disable-tls-check flag to configure
    Even though glib-networking is installed before libsoup,
    the configure phase of the latter is complaining about
    it. And the issue is not about having a too old version
    of the dependency, because libsoup's check doesn't look
    for the version number in its configure.ac[0].
    
    But this is a runtime dependency, so we can pass this
    flag to silence the problem for now.
    
    (The libsoup package is only being used by the banshee
    profile for now.)
    
    [0] https://git.gnome.org/browse/libsoup/tree/configure.ac?id=LIBSOUP_2_33_90#n221

--- packages/libsoup.py
+++ packages/libsoup.py
@@ -3,7 +3,8 @@ class LibsoupPackage (GnomePackage):
 		GnomePackage.__init__ (self, 'libsoup', '2.33', '90')
 		self.configure_flags = [
 			'--disable-gtk-doc',
-			'--without-gnome'
+			'--without-gnome',
+			'--disable-tls-check',
 		]
 
 LibsoupPackage ()
