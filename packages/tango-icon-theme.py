b814b2c881f99b2f6f593d4b1017c69d3f82de74
Andrés G. Aragoneses <knocte@gmail.com>
2015-06-28 14:27:13 +0200

    [tango-icon-theme] workaround DESTDIR honouring bug
    tango-icon-theme or icon-naming-utils don't seem to
    honour DESTDIR properly (reported bug to upstream as
    https://bugs.freedesktop.org/show_bug.cgi?id=91134).
    
    Then a workaround is placed as a hook to the install
    method.

--- packages/tango-icon-theme.py
+++ packages/tango-icon-theme.py
@@ -1,3 +1,17 @@
-FreeDesktopPackage ('tango', 'tango-icon-theme', '0.8.90', configure_flags = [
-	'--disable-icon-framing'
-])
+class TangoIconThemePackage (FreeDesktopPackage):
+        def __init__ (self):
+		FreeDesktopPackage.__init__ (self, 'tango', 'tango-icon-theme', '0.8.90',
+			configure_flags  = [
+				'--disable-icon-framing'
+			]
+		)
+
+	def install (self):
+		# workaround https://bugs.freedesktop.org/show_bug.cgi?id=91134
+		self.package_prefix = self.profile.prefix
+		self.stage_root = self.profile.stage_root
+		# see http://cgit.freedesktop.org/gstreamer-sdk/icon-naming-utils/tree/icon-name-mapping.pl.in?h=sdk-0.8.7
+		self.makeinstall = 'INU_DATA_DIR=%{stage_root}%{package_prefix}/share/icon-naming-utils make install DESTDIR=%{stage_root}'
+		Package.install (self)
+
+TangoIconThemePackage()
