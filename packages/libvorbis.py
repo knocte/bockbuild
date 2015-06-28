48638962d2c481534c765b70c812a95ab59f0f76
Andrés G. Aragoneses <knocte@gmail.com>
2015-06-29 01:42:14 +0200

    [libvorbis] Fix custom configure method
    This custom configure was using the %{prefix} variable,
    which seems to be gone in this version of bockbuild,
    and now self.profile.prefix needs to be used.
    
    Otherwise this build error would appear:
    (bockbuild error) 'prefix' could not be resolved in string

--- packages/libvorbis.py
+++ packages/libvorbis.py
@@ -4,7 +4,9 @@ class LibvorbisPackage (XiphPackage):
                         project  = 'vorbis',
 			name = 'libvorbis',
                         version = '1.3.2')
-		self.configure = './configure --prefix="%{prefix}"'
+
+		self.package_prefix = self.profile.prefix
+		self.configure = './configure --prefix="%{package_prefix}"'
 
 		# reduce optimization from -O4 to -O3 to allow compilation on Xcode 4.2.1
 		self.sources.append ('patches/libvorbis-opt.patch')
