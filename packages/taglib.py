44f50dc27b6c39d0a5bee70ddea0c3928608cb99
David Nielsen <gnomeuser@gmail.com>
2015-06-29 04:56:12 +0200

    [taglib] fix build, upgrading to 1.8
    Build of older version of taglib was failing[0],
    and bringing this new version fixes the problem.
    
    CMake is added as a dependency because this new
    version of taglib depends on this build system.
    
    taglib is only being used in the banshee profile.
    
    [0] clang: error: no such file or directory: 'g++'

--- packages/taglib.py
+++ packages/taglib.py
@@ -1,3 +1,19 @@
-Package ('taglib', '1.6.3', sources = [
-	'http://developer.kde.org/~wheeler/files/src/%{name}-%{version}.tar.gz'
-])
+class TaglibPackage (Package):
+	def __init__ (self):
+		Package.__init__ (self, 'taglib', '1.8',
+			sources = [
+				'http://taglib.github.io/releases/%{name}-%{version}.tar.gz'
+			])
+
+	def build (self):
+		self.sh (
+			# Let's set a bunch of variables, to make cmake feel loved
+			'CMAKE_PREFIX_PATH=%{prefix}',
+			'CMAKE_LIBRARY_PATH=%{prefix}/lib',
+			'CMAKE_INSTALL_PREFIX=%{prefix}',
+
+			# build and install
+			'cmake -DCMAKE_INSTALL_PREFIX=%{prefix} . && make all install',
+			)
+
+TaglibPackage ()
--- profiles/banshee/packages.py
+++ profiles/banshee/packages.py
@@ -14,6 +14,7 @@ class BansheePackages:
 			'libtool.py',
 			'gettext.py',
 			'pkg-config.py',
+			'cmake.py',
 		])
 
 		# Base Libraries
